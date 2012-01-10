b994650e9e9b6b58878ce45021c59914ff116098
richi chen <richi.chen@gmail.com>
2012-01-11 01:19:02 +0800

    remove empty device/ti/panda/proprietary-open/bt/omap4/system/etc/firmware/ti-connectivity folder copy

--- proprietary-open/install-binaries.mk
+++ proprietary-open/install-binaries.mk
@@ -3,7 +3,6 @@ PRODUCT_COPY_FILES:= \
 	device/ti/panda/proprietary-open/bt/omap4/system/etc/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
 	device/ti/panda/proprietary-open/bt/omap4/system/etc/firmware/fmc_ch8_1273.2.bts:system/etc/firmware/fmc_ch8_1273.2.bts \
 	device/ti/panda/proprietary-open/bt/omap4/system/etc/firmware/fm_rx_ch8_1273.2.bts:system/etc/firmware/fm_rx_ch8_1273.2.bts \
-	device/ti/panda/proprietary-open/bt/omap4/system/etc/firmware/ti-connectivity:system/etc/firmware/ti-connectivity \
 	device/ti/panda/proprietary-open/wifi/omap4/system/etc/firmware/ti-connectivity/wl128x-fw.bin:system/etc/firmware/ti-connectivity/wl128x-fw.bin \
 	device/ti/panda/proprietary-open/wifi/omap4/system/etc/firmware/ti-connectivity/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
 	device/ti/panda/proprietary-open/wifi/omap4/system/etc/firmware/ti-connectivity/wl1271-nvs-example-wl128x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs-example-wl128x.bin \
