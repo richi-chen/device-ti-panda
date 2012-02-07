bfa0f09578b178f9b045b31904faf3ac5cd7d256
richi chen <richi.chen@gmail.com>
2012-02-07 18:40:38 +0800

    Make tf_daemon service oneshot, new bt/fm firmware provided by omapzoom, README update

--- proprietary-open/install-binaries.mk
+++ proprietary-open/install-binaries.mk
@@ -1,6 +1,4 @@
 PRODUCT_COPY_FILES:= \
-	device/ti/richi-panda/proprietary-open/bt/omap4/system/etc/firmware/TIInit_7.2.31.bts:system/etc/firmware/TIInit_7.2.31.bts \
-	device/ti/richi-panda/proprietary-open/bt/omap4/system/etc/firmware/TIInit_7.6.15.bts:system/etc/firmware/TIInit_7.6.15.bts \
 	device/ti/richi-panda/proprietary-open/bt/omap4/system/etc/firmware/fmc_ch8_1273.2.bts:system/etc/firmware/fmc_ch8_1273.2.bts \
 	device/ti/richi-panda/proprietary-open/bt/omap4/system/etc/firmware/fm_rx_ch8_1273.2.bts:system/etc/firmware/fm_rx_ch8_1273.2.bts \
 	device/ti/richi-panda/proprietary-open/wifi/omap4/system/etc/firmware/ti-connectivity/wl1271-nvs.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin \
